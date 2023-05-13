87fb40ed261b0b512dd4f32212f53281a2a7e719
Pedro Henrique Guedes <kropinho211@outlook.com>
2023-05-13 10:36:53 -0300

    "Possible moves of a piece"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,7 +16,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         
-        while(true){ 
+        while(true){
+        try{
+        UI.clearScreen();
         UI.printBoard(chessMatch.getPieces());
         System.out.println();
         System.out.print("Origem: ");
@@ -25,7 +29,15 @@ public class Main {
         ChessPosition target = UI.readChessPosition(sc);
 
         ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-
+        }
+        catch(ChessException e){
+            System.out.println(e.getMessage());
+            sc.nextLine();
+        }
+        catch(InputMismatchException e){
+            System.out.println(e.getMessage());
+            sc.nextLine();
+        }
 
     }
         
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -14,6 +14,23 @@ public class Piece {
         return board;
     }
 
+    public abstract boolean[][] possibleMoves();
+
+        public boolean possibleMove(Position position){
+            return possibleMoves()[position.getRows()][position.getColumns()]; // retorna a matriz de movimentos possíveis na posição
+        }
+
+        public boolean isThereAnyPossibleMove(){
+            boolean[][] mat = possibleMoves();
+            for(int i = 0; i < mat.length; i++){
+                for(int j = 0; j < mat.length; j++){
+                    if(mat[i][j]){
+                        return true;
+                    }
+                }
+            }
+            return false;
+    }
 
 
     
